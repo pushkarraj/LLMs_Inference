0f64439bd560188abd12745ec2e3495f80262490
pushkarraj <pushkar.raj6@gmail.com>
2023-03-31 20:09:37 +0530

    stable

--- inference.py
+++ inference.py
@@ -9,11 +9,12 @@ tokenizer.add_bos_token = False
 # Load the model. Alpa automatically downloads the weights to the specificed path
 model = get_model(model_name="alpa/opt-2.7b", path="~/opt_weights/")
 
+start=time.time()
 # Generate
 prompt = "Paris is the capital city of"
 
 input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 output = model.generate(input_ids=input_ids, max_length=256, do_sample=True)
 generated_string = tokenizer.batch_decode(output, skip_special_tokens=True)
-
-print(generated_string)
+end=time.time()
+print(generated_string,end-start)
